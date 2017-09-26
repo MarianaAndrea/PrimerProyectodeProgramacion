6b52bbce7b1c0691e8d5e0641568963f630cf2cf
MarianaParedes <mariana.paredes@softlogia.com>
2017-09-26 00:12:17 -0300

    Subiendo el proyecto

---
+++ PrimerProyectodeProgramacion/Electrodomestico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimerProyectodeProgramacion
+{
+    class Electrodomestico
+    {
+        private float precio_base = 0;
+        //color de tipo coleccion
+        //consumo_energetico
+        //peso expresado en kilos
+    }
+}
